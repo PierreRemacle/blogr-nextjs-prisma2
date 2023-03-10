8513595b1e671d9fe6a4c8b7057bd75788a753b2
piremacle <Pierrepierreremalce@gmail.com>
2023-03-10 23:09:24 +0100

    website V2

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,8 @@ export default function FirstPost() {
         </div>
         <div className={styles.tree}>
 
-            <h1> Equipe </h1>
+            <h1> EQUIPE </h1>
+            
             <h2> Notre équipe est constituée d’étudiants universitaires dans le domaine des sciences exactes. </h2>
             <h2>  Notre mission est de transmettre nos connaissances et notre passion pour les sciences à la nouvelle génération. Nous désirons offrir à tout étudiant l’opportunité de réussir dans ces matières complexes.</h2>
             <h2> En tant qu'association, nous nous engageons à préparer les jeunes pour qu'ils réussissent et se construisent un avenir prometteur.</h2>
@@ -55,9 +56,10 @@ export default function FirstPost() {
                     <div className={styles.pright}>
                         <p> Nous accueillons les élèves pour répondre à leurs questions et les aider dans la compréhension de leurs devoirs et leurs leçons </p>
                         <br></br>
+                        
                         <ul> <FontAwesomeIcon icon={faLocation}/> Où? Collège Saint-Croix Hannut. </ul>
-                        <ul> <FontAwesomeIcon icon={faCalendarDay}/> Quand? les samedis matin (10-13h) </ul>
-                        <ul> <FontAwesomeIcon icon={faChalkboardTeacher}/> Comment? les cours sont donné par groupe de 4 </ul>
+                        <ul> <FontAwesomeIcon icon={faCalendarDay}/> Quand? Les samedis matin (10-13h) </ul>
+                        <ul> <FontAwesomeIcon icon={faChalkboardTeacher}/> Comment? Les cours sont donné par groupe de 4 </ul>
                         <ul> <FontAwesomeIcon icon={faMoneyBill}/> Tarif? 35€/3h </ul>
                         <br></br>
                         <div className={styles.buttonContainer}>
@@ -75,13 +77,13 @@ export default function FirstPost() {
                 <ul> 
                     
                     <p> 
-                    <a href="mailto:communication@rework.education" title="Email" className={styles.lables2}><FontAwesomeIcon icon={faEnvelope}/></a><span className={styles.lables}>Email:</span> <a href="mailto:communication@rework.education" className={styles.lables1}>communication@rework.education</a>
+                    <a href="mailto:communication@rework.education" title="Email" className={styles.lables2}><FontAwesomeIcon icon={faEnvelope}/></a><span className={styles.lables}>  Email:</span> <a href="mailto:communication@rework.education" className={styles.lables1}>communication@rework.education</a>
                     </p>
                     <p> 
                     <a href="tel:0493033415" title="Téléphone" className={styles.lables2}><FontAwesomeIcon icon={faPhone}/></a><span className={styles.lables}>Téléphone:</span> <a href="tel:0493033415" className={styles.lables1}>0493/033415</a>
                     </p>
                     <p> 
-                    <a href="https://www.facebook.com/profile.php?id=100090766090539" title="Facebook" className={styles.lables2}><FontAwesomeIcon icon={faFacebook}/></a><span className={styles.lables}>Facebook:</span> <a href="https://www.facebook.com/profile.php?id=100090766090539" className={styles.lables1}>Rework</a>  
+                    <a href="https://www.facebook.com/profile.php?id=100090766090539" title="Facebook" className={styles.lables2}><FontAwesomeIcon icon={faFacebook}/></a><span className={styles.lables}>  Facebook:</span> <a href="https://www.facebook.com/profile.php?id=100090766090539" className={styles.lables1}>Rework</a>  
                     </p>
                 </ul>
                 </div>
